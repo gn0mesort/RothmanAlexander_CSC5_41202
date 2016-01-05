e869ece6a62003200e0ad3d5ec989c1937e9be70
gn0mesort <arothman@student.rcc.edu>
2016-01-05 11:46:18 -0800

    Signed-off-by: gn0mesort <arothman@student.rcc.edu>

---
+++ Class/NoMistake/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexander Rothman
+ * Purpose: Check out IDE, Hello, World!
+ * Created on January 5, 2016, 11:13 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    short a = 30000, b = 20000, c;
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    c = a + b;
+    //Output the results
+    cout << "A = " << a << endl;
+    cout << "B = " << b << endl;
+    cout << "C = " << c << endl;
+    cout << c << " = " << a << " + " << b << endl;
+    
+    //Exit stage right
+    return 0;
+}
+
---
+++ Homework/Savitch_8th_Edition_Chapter1_Problem10/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexander Rothman
+ * Purpose: Big C
+ * Created on January 5, 2016, 11:40 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    char letter = 'X';
+    //Input data
+
+    //Calculate or map inputs to outputs
+    
+    //Output the results
+    cout  << "  " << letter << letter << letter << endl;
+    cout << ' ' << letter << "   " << letter << endl;
+    cout << letter << endl;
+    cout << letter << endl;
+    cout << letter << endl;
+    cout << letter << endl;
+    cout << letter << endl;
+    cout << ' ' << letter << "   " << letter << endl;
+    cout  << "  " << letter << letter << letter << endl;
+    //Exit stage right
+    return 0;
+}
+

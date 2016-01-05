cc9c22a1eaf1aea84d576690d72a03f26d3eb921
gn0mesort <arothman@student.rcc.edu>
2016-01-05 10:51:55 -0800

    Signed-off-by: gn0mesort <arothman@student.rcc.edu>

---
+++ Class/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexander Rothman
+ * Purpose: Check out IDE
+ * Created on January 4, 2016, 10:18 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    
+    //Output the results
+    cout << "Hello, World!" << endl;
+    
+    //Exit stage right
+    return 0;
+}
+

3b11e3938cec7d902ea2ba6db2604a31f2374c2f
gn0mesort <arothman@student.rcc.edu>
2016-01-13 05:48:15 -0800

    Signed-off-by: gn0mesort <arothman@student.rcc.edu>

--- Lab/Lab2/Marks_Law_Ternary/nbproject/Makefile-variables.mk
+++ Lab/Lab2/Marks_Law_Ternary/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=marks_law_ternary
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/marks_law_ternary
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/marks_law_ternary
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=markslawternary.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/markslawternary.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/markslawternary.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=marks_law_ternary
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/marks_law_ternary
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/marks_law_ternary
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=markslawternary.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/markslawternary.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/markslawternary.tar
 #
 # include compiler specific variables
 #

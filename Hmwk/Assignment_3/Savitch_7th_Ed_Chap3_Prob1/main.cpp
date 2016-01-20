e69b38b05b224d96d44461d60fade813821af69b
gn0mesort <arothman@student.rcc.edu>
2016-01-20 09:47:48 -0800

    Signed-off-by: gn0mesort <arothman@student.rcc.edu>

--- Hmwk/Assignment_3/Savitch_7th_Ed_Chap3_Prob1/main.cpp
+++ Hmwk/Assignment_3/Savitch_7th_Ed_Chap3_Prob1/main.cpp
@@ -19,33 +19,59 @@ using namespace std;
 //Function Prototypes
 
 //Begin Execution
+
 int main(int argc, char** argv) {
     //Declaration and Initialization
     char comp, //The CPUs choice for this turn
-         player; //The player's choice for this turn
- 
-    srand(static_cast<int>(time(0))); //Seed the random number generator
-    
+            player, //The player's choice for this turn
+            qstion; //Does the player want to continue playing?
+    unsigned short pScore = 0,
+                   cScore = 0;
+
+    srand(static_cast<int> (time(0))); //Seed the random number generator
+
     //Output Header
     cout << "Solution to Savitch 7th Ed. Chap3 Prob1" << endl;
     cout << "The ROCK-PAPER-SCISSORS Game" << endl;
-    
+
     //Input Data
-    do{
-        cout << "What is your move P, R, S? ";
-        cin  >> player;
-        player = toupper(player);
-    }while(!(player == 'P' || player == 'R' || player == 'S'));
+    do {
+        do {
+            cout << "What is your move P, R, S? ";
+            cin >> player;
+            player = toupper(player);
+        } while (!(player == 'P' || player == 'R' || player == 'S'));
+
+        //Computer Generated Play
+        do {
+            comp = rand() % 4 + 80;
+        } while (comp == 'Q');
+
+        //Output Results
+        //Output the moves by the computer and player
+        cout << "The computer played: " << comp << endl;
+        cout << "The player played:   " << player << endl;
+        //Determine results
+        if (player == comp) {
+            cout << "RESULT: TIE!!" << endl;
+        } else if ((player == 'P' && comp == 'R') || (player == 'R' && comp == 'S')
+                || (player == 'S' && comp == 'P')) {
+            cout << "RESULT: PLAYER WINS!!" << endl;
+            ++pScore;
+        } else {
+            cout << "RESULT: COMPUTER WINS!!" << endl;
+            ++cScore;
+        }
+        //Show Scores
+        cout << "PLAYER SCORE:   " << pScore << endl;
+        cout << "COMPUTER SCORE: " << cScore << endl;
+        //Keep playing?
+        cout << "Quit Playing? (Y/N) ";
+        cin >> qstion;
+        qstion = toupper(qstion);
+        cout << endl;
+    } while (qstion != 'Y');
     
-    //Computer Generated Play
-    do{
-        comp = rand() % 4 + 80;
-    }while(comp == 'Q');
-    
-    //Output Results
-    cout << "The computer played: " << comp << endl;
-    cout << "The player played:   " << player << endl;
-       
     //Exit
     return 0;
 }

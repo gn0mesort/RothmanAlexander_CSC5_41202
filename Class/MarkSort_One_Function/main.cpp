2772b5407ecafcd67449df4c0c5b3c0615457fb0
gn0mesort <arothman@student.rcc.edu>
2016-02-02 09:17:37 -0800

    Signed-off-by: gn0mesort <arothman@student.rcc.edu>

---
+++ Class/MarkSort_One_Function/main.cpp
@@ -0,0 +1,98 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexander Rothman
+ * Purpose: To develop a sorting routine called MarkSort
+ * Created on February 2, 2016, 8:18 AM
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+void fillAry(int [], int);
+void prntAry(int [], int, int);
+void markSrt(int[], int);
+
+//Begin Execution
+
+int main(int argc, char** argv) {
+    //Declaration and Initialization
+    const int SIZE = 100;
+    int arr[SIZE];
+
+    srand(static_cast<unsigned int> (time(0)));
+
+    fillAry(arr, SIZE); //Fill array
+    prntAry(arr, SIZE, 10); //Print array
+    markSrt(arr, SIZE); //Test sort
+    prntAry(arr, SIZE, 10); //Print sorted array
+
+    //Exit
+    return 0;
+}
+
+/******************************************************************************/
+/*******************************Fill Array*************************************/
+/******************************************************************************/
+//Input
+//  arr : the array to fill
+//  length : the length of the array
+//Output
+//  arr : the filled array
+
+void fillAry(int arr[], int length) {
+    //loop and fill the array with random numbers
+    for (int i = 0; i < length; ++i) {
+        arr[i] = rand() % 90 + 10;
+    }
+}
+
+/******************************************************************************/
+/******************************Print Array*************************************/
+/******************************************************************************/
+//Input
+//  arr : the array to print
+//  length : the length of the array
+
+void prntAry(int arr[], int length, int perLine) {
+    //loop and output array
+    cout << endl;
+    for (int i = 0; i < length; ++i) {
+        if (i % perLine == 0) {
+            cout << endl;
+        }
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+/******************************************************************************/
+/********************************Mark Sort*************************************/
+/******************************************************************************/
+//Input
+//  arr : the array to sort
+//  length : the length of the array
+//Output
+//  arr : the sorted array
+
+void markSrt(int arr[], int length) {
+    for (int i = 0; i < length - 1; ++i) {
+        //loop and compare
+        for (int j = i + 1; j < length; ++j) {
+            if (arr[i] > arr[j]) {
+                //swap one value with another
+                arr[i] = arr[i] ^ arr[j];
+                arr[j] = arr[i] ^ arr[j];
+                arr[i] = arr[i] ^ arr[j];
+            }
+        }
+    }
+}
